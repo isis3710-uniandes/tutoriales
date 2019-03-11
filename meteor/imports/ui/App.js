a93fdce725db607b3f8ac769a0bf82077c536467
Fabian Peña <fc.pena@uniandes.edu.co>
2019-03-11 00:07:44 -0500

    Meteor tutorial sources added

---
+++ meteor/imports/ui/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+
+import { Tasks } from '../api/tasks.js';
+
+import Task from './Task.js';
+ 
+// App component - represents the whole app
+class App extends Component {
+   
+  renderTasks() {
+    return this.props.tasks.map((task) => (
+      <Task key={task._id} task={task} />
+    ));
+  }
+ 
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+ 
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default withTracker(() => {
+  return {
+    tasks: Tasks.find({}).fetch(),
+  };
+})(App);
